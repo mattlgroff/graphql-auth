5bf61aa82747b6e1d5e23139ba5ff80b8dfb6b31
Matthew Groff <matthew.groff@accenture.com>
2019-09-26 11:55:26 -0500

    GraphQL with JsonWebToken sample finished

--- schema/index.js
+++ schema/index.js
@@ -1,27 +1,94 @@
 const { AuthenticationError, gql } = require('apollo-server-express');
+const { authenticateGoogle } = require('../passport');
+const jwt = require('jsonwebtoken');
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
     hello: String
-    user: String
+    user: UserToken
+  }
+
+  type UserToken {
+    name: String
+    email: String
+  }
+
+  type AuthResponse {
+    token: String
+    name: String
+  }
+
+  input AuthInput {
+    accessToken: String!
+  }
+
+  type Mutation {
+    authGoogle(input: AuthInput!): AuthResponse
   }
 `;
-
-// Resolver Authentication
-const requiresLogin = resolver => (parent, args, context, info) => {
-  if (context.user) {
-    return resolver(parent, args, context, info);
-  } else {
-    throw new AuthenticationError('Unauthorized');
-  }
-};
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    user: requiresLogin((parent, args, { user }) => user),
+    user: (parent, args, { user }) => {
+      const { name, email } = user;
+
+      return {
+        name,
+        email,
+      };
+    },
     hello: () => 'hello',
+  },
+  Mutation: {
+    authGoogle: async (parent, { input }, { req, res }) => {
+      req.body = {
+        ...req.body,
+        access_token: input.accessToken,
+      };
+
+      try {
+        const user = {
+          name: 'Matthew Groff',
+          email: 'mattlgroff@gmail.com',
+        };
+
+        return {
+          name: user.name,
+          token: jwt.sign(user, process.env.JWT_SECRET),
+        };
+        // data contains the accessToken, refreshToken and profile from passport
+        // const { data, info } = await authenticateGoogle(req, res);
+
+        // if (data) {
+        // const user = {
+        //   name: 'Matthew Groff',
+        //   email: 'mattlgroff@gmail.com',
+        // };
+
+        //   if (user) {
+        //     return {
+        //       name: user.name,
+        //       token: jwt.sign(user, process.env.JWT_SECRET),
+        //     };
+        //   }
+        // }
+
+        // if (info) {
+        //   console.log(info);
+        //   switch (info.code) {
+        //     case 'ETIMEDOUT':
+        //       return new Error('Failed to reach Google: Try Again');
+        //     default:
+        //       return new Error('something went wrong');
+        //   }
+        // }
+        // return Error('server error');
+      } catch (error) {
+        return error;
+      }
+    },
   },
 };
 
--- server.js
+++ server.js
@@ -1,20 +1,33 @@
+require('dotenv').config();
 const express = require('express');
-const passport = require('passport');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schema');
+const jwt = require('jsonwebtoken');
 
 // Setup Express App
 const app = express();
-
-// Setup Passport
-// TODO: Implement Passport Strategy for Google Oauth
-app.use(passport.initialize());
-app.use(passport.session());
 
 // Setup ApolloServer
 const apollo = new ApolloServer({
   typeDefs,
   resolvers,
+  context: context => {
+    const token = context.req.headers.authorization || '';
+    const user = jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      // Don't Authentication authGoogle Mutation Used For Getting Token
+      const isWhitelisted = context.req.body.query.includes('authGoogle');
+
+      if (err && !isWhitelisted) {
+        throw new AuthenticationError('Unauthorized');
+      }
+      return decoded;
+    });
+
+    return {
+      ...context,
+      user,
+    };
+  },
 });
 apollo.applyMiddleware({ app });
 
